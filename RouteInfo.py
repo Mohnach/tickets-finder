e828be112edea02de08c8d78ef52f6c4cce288ab
Mohnach <alt.novikov@gmail.com>
2020-03-20 01:54:17 +0300

    added default values and properties for datetime

--- RouteInfo.py
+++ RouteInfo.py
@@ -1,13 +1,36 @@
 from dataclasses import dataclass
-from datetime import date, time
+from datetime import date, time, datetime
+from decimal import Decimal
 
 @dataclass
 class RouteInfo:
-    origin_city: str
-    destination_city: str
-    depart_date: date
-    depart_time: time
-    arrival_date: date
-    arrival_time: time
-    route_type: str
-    price: float
+    origin_city: str = ''
+    destination_city: str = ''
+    route_type: str = ''
+    depart_datetime: datetime = datetime.now()
+    arrival_datetime: datetime = datetime.now()
+    price: Decimal = Decimal('0')
+
+    @property
+    def depart_date(self) -> date:
+        return self.depart_datetime.date()
+
+    @property
+    def depart_time(self) -> time:
+        return self.depart_datetime.time()
+
+    @property
+    def arrival_date(self) -> date:
+        return self.arrival_datetime.date()
+
+    @property
+    def arrival_time(self) -> time:
+        return self.arrival_datetime.time()
+
+if __name__ == "__main__":
+    test = RouteInfo('Moscow','London', 'peshkom')
+    print(test)
+    print(test.arrival_time)
+    print(test.depart_date)
+    test2 = datetime(1970,4,11)
+    print(test2)
