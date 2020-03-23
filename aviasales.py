4773b88fc73c4271d09f99832f3c3e2b8c428d27
Mohnach <alt.novikov@gmail.com>
2020-03-23 22:18:19 +0300

    moved aviasales functions into TicketProvider class

--- aviasales.py
+++ aviasales.py
@@ -1,128 +1,152 @@
 import requests
 import secrets
+from decimal import Decimal
+from datetime import datetime
+from TicketProvider import TicketProvider
+from RouteInfo import RouteInfo
+from dataclasses import dataclass
+from typing import List
 
-def get_iata_from_dict(iata_dict, target):
-    if target in iata_dict:
-        if 'iata' in iata_dict[target]:
-            return iata_dict[target]['iata']
-    raise ValueError(f'IATA of {target} not found')
+class Aviasales(TicketProvider):
 
-def convert_city_name_to_iata(origin_name, dest_name):
-    
-    search = 'Из ' + origin_name + ' в ' + dest_name
+    def get_tickets(self, origin : str, destination : str, depart_date : datetime) -> List[RouteInfo]:
+        pass
 
-    iata_url = 'https://www.travelpayouts.com/widgets_suggest_params'
-    params = {
-        'q' : search
-    }
-    
-    try:
-        result = requests.get(iata_url, params=params)
-        result.raise_for_status()
-        json_result = result.json()
+    def get_iata_from_dict(self, iata_dict, target):
+        if target in iata_dict:
+            if 'iata' in iata_dict[target]:
+                return iata_dict[target]['iata']
+        raise ValueError(f'IATA of {target} not found')
 
-        iatas = {}
-        iatas['origin'] = get_iata_from_dict(json_result, 'origin')
-        iatas['destination'] = get_iata_from_dict(json_result, 'destination')
+    def convert_city_name_to_iata(self, origin_name, dest_name):
+        
+        search = 'Из ' + origin_name + ' в ' + dest_name
 
-        return iatas
+        iata_url = 'https://www.travelpayouts.com/widgets_suggest_params'
+        params = {
+            'q' : search
+        }
+        
+        try:
+            result = requests.get(iata_url, params=params)
+            result.raise_for_status()
+            json_result = result.json()
 
-    except (requests.RequestException, ValueError):
-        print('Cities name conversion failed')
-        raise ValueError
+            iatas = {}
+            iatas['origin'] = self.get_iata_from_dict(json_result, 'origin')
+            iatas['destination'] = self.get_iata_from_dict(json_result, 'destination')
+
+            return iatas
+
+        except (requests.RequestException, ValueError):
+            print('Cities name conversion failed')
+            raise ValueError
 
 
-def avia_tickets_by_city(origin_city, destination_city, depart_date, return_date):
+    def avia_tickets_by_city_week_matrix(self, origin_city, destination_city, depart_date, return_date):
 
-    iatas = convert_city_name_to_iata(origin_city, destination_city)
+        iatas = convert_city_name_to_iata(origin_city, destination_city)
 
-    try:
-        destination_iata = iatas.get('destination')
-        origin_iata = iatas.get('origin')
-    except ValueError:
-        print('Invalid city name')
+        try:
+            destination_iata = iatas.get('destination')
+            origin_iata = iatas.get('origin')
+        except ValueError:
+            print('Invalid city name')
+            return None
+        
+        tickets_url = 'http://api.travelpayouts.com/v2/prices/week-matrix'
+        params = {
+            'currency' : 'rub',
+            'origin' : origin_iata,
+            'destination' : destination_iata,
+            'show_to_affiliates' : 'false',
+            'depart_date' : depart_date,
+            'return_date' : return_date,
+            'token' : secrets.travelpayouts_token
+        }
+        try:
+            result = requests.get(tickets_url, params=params)
+            result.raise_for_status()
+            json_result = result.json()
+
+            if 'success' in json_result:
+                if json_result['success'] == True:
+                    return json_result.get('data')
+        except (requests.RequestException, ValueError):
+            print('')
         return None
-    
-    tickets_url = 'http://api.travelpayouts.com/v2/prices/week-matrix'
-    params = {
-        'currency' : 'rub',
-        'origin' : origin_iata,
-        'destination' : destination_iata,
-        'show_to_affiliates' : 'false',
-        'depart_date' : depart_date,
-        'return_date' : return_date,
-        'token' : secrets.travelpayouts_token
-    }
-    try:
-        result = requests.get(tickets_url, params=params)
-        result.raise_for_status()
-        json_result = result.json()
-
-        if 'success' in json_result:
-            if json_result['success'] == True:
-                return json_result.get('data')
-    except (requests.RequestException, ValueError):
-        print('')
-    return None
 
 
-def avia_tickets_by_city_cheap(origin_city, destination_city, depart_date, return_date):
+    def get_return_tickets(self, origin : str, destination : str, depart_date : datetime, return_date : datetime) -> List[RouteInfo]:
+        iatas = self.convert_city_name_to_iata(origin_city, destination_city)
 
-    iatas = convert_city_name_to_iata(origin_city, destination_city)
+        try:
+            destination_iata = iatas.get('destination')
+            origin_iata = iatas.get('origin')
+        except ValueError:
+            print('Invalid city name')
+            return []
+        
+        tickets_url = 'http://api.travelpayouts.com/v1/prices/cheap'
+        params = {
+            'currency' : 'rub',
+            'origin' : origin_iata,
+            'destination' : destination_iata,
+            'depart_date' : depart_date,
+            'return_date' : return_date,
+            'token' : secrets.travelpayouts_token
+        }
+        try:
+            result = requests.get(tickets_url, params=params)
+            result.raise_for_status()
+            json_result = result.json()
 
-    try:
-        destination_iata = iatas.get('destination')
-        origin_iata = iatas.get('origin')
-    except ValueError:
-        print('Invalid city name')
-        return None
-    
-    tickets_url = 'http://api.travelpayouts.com/v1/prices/cheap'
-    params = {
-        'currency' : 'rub',
-        'origin' : origin_iata,
-        'destination' : destination_iata,
-        'depart_date' : depart_date,
-        'return_date' : return_date,
-        'token' : secrets.travelpayouts_token
-    }
-    try:
-        result = requests.get(tickets_url, params=params)
-        result.raise_for_status()
-        json_result = result.json()
+            if 'success' in json_result:
+                if json_result['success'] == True:
+                    return self.extract_tickets_from_json(json_result.get('data'), destination_iata, origin_city, destination_city)
+        except (requests.RequestException, ValueError):
+            pass
+        return []
 
-        if 'success' in json_result:
-            if json_result['success'] == True:
-                return normalize_data(json_result.get('data'), destination_iata, origin_city, destination_city)
-    except (requests.RequestException, ValueError):
-        print('')
-    return None
+    def extract_tickets_from_json(self, data, destination_iata, origin_city, destination_city):
+        tickets = []
+        tickets_list = data.get(destination_iata)
+        
+        for ticket_key in tickets_list:
+            ticket_value = tickets_list.get(ticket_key)
+            ticket = AviasalesInfo()
+            ticket.route_type = 'plane'
+            ticket.origin_city = origin_city
+            ticket.destination_city = destination_city
+            ticket.number = ticket_value.get('flight_number')
+            ticket.airline = ticket_value.get('airline')
+            ticket.price = Decimal(ticket_value.get('price'))
+            depart_str = ticket_value.get('departure_at')
+            ticket.depart_datetime = datetime.strptime( depart_str, '%Y-%m-%dT%H:%M:%SZ' )
 
-def normalize_data(data, destination_iata, origin_city, destination_city):
-    tickets = []
-    tickets_list = data.get(destination_iata)
-    ticket = {}
-    for ticket_key in tickets_list:
-        ticket_value = tickets_list.get(ticket_key)
-        ticket['type'] = 'plane'
-        ticket['origin'] = origin_city
-        ticket['destination'] = destination_city
-        ticket['number'] = ticket_value.get('flight_number')
-        ticket['airline'] = ticket_value.get('airline')
-        ticket['price'] = ticket_value.get('price')
-        ticket['departure_at'] = ticket_value.get('departure_at')
-        ticket['return_at'] = ticket_value.get('return_at')
+            return_str = ticket_value.get('return_at')
+            ticket.return_datetime = datetime.strptime( return_str, '%Y-%m-%dT%H:%M:%SZ' )
 
-        tickets.append(ticket)
+            tickets.append(ticket)
 
-    return tickets
+        return tickets
+
+# информация о маршруте на самолете
+@dataclass
+class AviasalesInfo(RouteInfo):
+    airline: str = ''
+
 
 if __name__ == "__main__":
     #city = input('Введите город: ')
     origin_city = 'Москва'
     destination_city = 'Сочи'
-    depart_date='2020-03-21'
-    return_date='2020-03-22'
+
+
+    depart_date = datetime.strptime('2020-04-21', '%Y-%m-%d')
+    return_date = datetime.strptime('2020-04-22', '%Y-%m-%d')
+
+    aviasales_parser = Aviasales()
     
     #print(avia_tickets_by_city(origin_city, destination_city, depart_date, return_date))
-    print(avia_tickets_by_city_cheap(origin_city, destination_city, depart_date, return_date))
+    print(aviasales_parser.get_return_tickets(origin_city, destination_city, depart_date, return_date))
