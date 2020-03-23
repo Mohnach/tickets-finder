231b9af4b9734dee6eadd85f9e1356f1379f917f
Mohnach <alt.novikov@gmail.com>
2020-03-23 22:14:27 +0300

    made dates fields optional. added return_datetime field

--- RouteInfo.py
+++ RouteInfo.py
@@ -1,31 +1,53 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from datetime import date, time, datetime
 from decimal import Decimal
+from typing import Optional
 
 @dataclass
 class RouteInfo:
     origin_city: str = ''
     destination_city: str = ''
     route_type: str = ''
-    depart_datetime: datetime = field(default_factory = lambda: datetime.now())
-    arrival_datetime: datetime = field(default_factory = lambda: datetime.now())
+    depart_datetime: Optional[datetime] = None
+    arrival_datetime: Optional[datetime] = None
+    return_datetime: Optional[datetime] = None
     price: Decimal = Decimal('0')
 
     @property
-    def depart_date(self) -> date:
-        return self.depart_datetime.date()
+    def depart_date(self) -> Optional[date]:
+        if self.depart_datetime is not None:
+            return self.depart_datetime.date()
+        return None
 
     @property
-    def depart_time(self) -> time:
-        return self.depart_datetime.time()
+    def depart_time(self) -> Optional[time]:
+        if self.depart_datetime is not None:
+            return self.depart_datetime.time()
+        return None
 
     @property
-    def arrival_date(self) -> date:
-        return self.arrival_datetime.date()
+    def arrival_date(self) -> Optional[date]:
+        if self.arrival_datetime is not None:
+            return self.arrival_datetime.date()
+        return None
 
     @property
-    def arrival_time(self) -> time:
-        return self.arrival_datetime.time()
+    def arrival_time(self) -> Optional[time]:
+        if self.arrival_datetime is not None:
+            return self.arrival_datetime.time()
+        return None
+
+    @property
+    def return_date(self) -> Optional[date]:
+        if self.return_datetime is not None:
+            return self.return_datetime.date()
+        return None
+
+    @property
+    def return_time(self) -> Optional[time]:
+        if self.return_datetime is not None:
+            return self.return_datetime.time()
+        return None
 
 if __name__ == "__main__":
     test = RouteInfo('Moscow','London', 'peshkom')
