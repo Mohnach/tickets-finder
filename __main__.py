bd541655d2f86f3dc006d554ab55db01270532a4
Mohnach <alt.novikov@gmail.com>
2020-04-18 20:21:23 +0300

    added city's name translation and calculation of the distance between airports

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from .tutu import Tutu
 from .aviasales import Aviasales
+from .CitiesInfo import CitiesInfo
 from .tickets_finder import prepare_db_session, get_return_tickets, get_tickets
+
 
 origin_city = 'Москва'
 destination_city = 'Сочи'
@@ -15,22 +17,36 @@ providers_list = []
 aviasales_provider = Aviasales(db_session=session)
 providers_list.append(aviasales_provider)
 
+# содержит инфу о городах: переводы на разные языки, iata code, координаты
+cities_info = CitiesInfo()
+eng_name = cities_info.translate_from_russian_to_english(origin_city)
+
+# поиск аэропортов для заданного города (возможно, лучше унести в CitiesInfo)
 aviasales_provider.load_airports_data()
-airports = aviasales_provider.find_airports_for_city('Moscow')
+airports = aviasales_provider.find_airports_for_city(eng_name)
 print(airports)
 
 aviasales_provider.load_flights_routes()
 
 tickets = []
 for airport in airports:
+    # поиск всех возможных направлений для заданного аэропорта
     routes_list = aviasales_provider.find_routes_for_airport(airport)
-    #routes_list = routes_list[1:10]
-    #print(routes_list)
+    print('Число маршрутов из аэропорта {}: {}'.format(airport, len(routes_list)))
 
     for route in routes_list:
-         #print('from {} to {}'.format(airport, route['Destination airport']))
-         tickets += aviasales_provider.get_return_tickets(airport, route['Destination airport'],
-               depart_date, return_date, convert_to_iata=False)
+        # print('from {} to {}'.format(airport, route['Destination airport']))
+        
+        # Если дистанция между аэропортами больше 5000км, то пропускаем это направление
+        distance = cities_info.calculate_distance(airport, route['Destination airport'])
+        if distance > 5000:
+            continue
+
+        tickets += aviasales_provider.get_return_tickets(airport,
+                                                         route['Destination airport'],
+                                                         depart_date,
+                                                         return_date,
+                                                         convert_to_iata=False)
 print(tickets)
 
 #tutu_provider = Tutu(db_session=session)
