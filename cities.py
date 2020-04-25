6cc101385cc25fbd2cdc67cb2159affffa268971
Mohnach <alt.novikov@gmail.com>
2020-04-25 19:31:49 +0300

    fixed accessing to a nonexistent element

--- cities.py
+++ cities.py
@@ -28,8 +28,7 @@ class Cities:
     def translate_from_russian_to_english(self, russian_name):
         for row in self.cities:
             if row['name'] == russian_name:
-                eng_name = row['name_translations']['en']
-        return eng_name
+                return row['name_translations'].get('en')
 
     def translate_from_iata_to_english(self, iata):
         for row in self.cities:
